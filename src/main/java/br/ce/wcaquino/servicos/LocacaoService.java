4008ecc97635c9595f20767f8047cea31ffef5e0
leobuck <leobuck12@gmail.com>
2023-07-24 13:38:39 -0300

    first commit

---
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -0,0 +1,71 @@
+package br.ce.wcaquino.servicos;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
+import br.ce.wcaquino.exceptions.LocadoraException;
+import br.ce.wcaquino.utils.DataUtils;
+
+public class LocacaoService {
+
+	public Locacao alugarFilme(Usuario usuario, List<Filme> filmes) throws FilmeSemEstoqueException, LocadoraException {
+		Double valorTotal = 0d;
+		
+		if (usuario == null) {
+			throw new LocadoraException("Usuario vazio");
+		}
+		
+		if (filmes == null || filmes.isEmpty()) {
+			throw new LocadoraException("Filmes vazio");
+		}
+		
+		for (Filme filme : filmes) {
+		
+			if (filme == null) {
+				throw new LocadoraException("Filme vazio");
+			}
+			
+			if (filme.getEstoque() == 0) {
+				//throw new Exception("Filme sem estoque");
+				throw new FilmeSemEstoqueException();
+			}
+		}
+		
+		Locacao locacao = new Locacao();
+		locacao.setFilmes(filmes);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		for (int i = 0; i < filmes.size(); i++) {
+			Filme filme = filmes.get(i);
+			Double valorFilme = filme.getPrecoLocacao();
+			switch (i) {
+				case 2: valorFilme *= 0.75; break;
+				case 3: valorFilme *= 0.5; break;
+				case 4: valorFilme *= 0.25; break;
+				case 5: valorFilme = 0d; break;
+			}
+			valorTotal += valorFilme;
+		}
+		locacao.setValor(valorTotal);
+
+		// Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		if (DataUtils.verificarDiaSemana(dataEntrega, Calendar.SUNDAY))
+			dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+
+		// Salvando a locacao...
+		// TODO adicionar método para salvar
+
+		return locacao;
+	}
+
+}
